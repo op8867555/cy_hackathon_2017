67120b8f95dcbe596ee1e7ad2f12a83166f6099f
Alex Lu <op8867555@gmail.com>
2017-07-16 13:56:29 +0800

    調整前端頭像

--- static/app.js
+++ static/app.js
@@ -5,7 +5,9 @@ function insertChat(who, text, time = 0){
     var elem =
         '<li class="mdl-list__item mdl-list__item--three-line">' +
           '<span class="mdl-list__item-primary-content">' +
-            '<i class="material-icons mdl-list__item-avatar">person</i>' +
+            '<i class="material-icons mdl-list__item-avatar">' +
+             ((who == "me") ? 'face' : 'person') +
+            '</i>' +
             '<span>' + ((who == "me") ? '自己' : '機器人')+ '</span>' +
               '<span class="mdl-list__item-text-body">' +
                  x +
